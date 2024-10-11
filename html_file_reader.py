1dc8e1870bf7c9ffa2488a5557480b0e8b628edf
RenanBa <renanbarbosadesouza@gmail.com>
2024-10-11 16:21:03 -0700

    Make two functions private

--- html_file_reader.py
+++ html_file_reader.py
@@ -3,10 +3,6 @@ import os
 
 
 class HtmlFileReader():
-    def print_test(self):
-        print("METHOD FROM ANOTHER FILE")
-        return True
-
     def find_html_file(self, search_dir):
         print("Searching for HML files...")
         current_dir_list = os.listdir(search_dir)
@@ -20,7 +16,19 @@ class HtmlFileReader():
                     html_files.append(f"{search_dir}/{item_type[0]}.{item_type[1]}")
         return html_files
 
-    def search_class_in_line(self, line):
+    def read_file_and_collect_att(self, file_path):
+        att_found_html = []
+        with open(file_path, mode='r',encoding='UTF-8') as html_file:
+            for line in html_file:
+                if "class" in line:
+                    att_found_html.append(self.__search_class_in_line(line))
+                if "id=" in line:
+                    att_found_html.append(self.__search_id_in_line(line))
+            print(f"HTML file ({file_path}) scanned for classes and ids.")
+        new_class_list = sum(att_found_html, [])
+        return new_class_list
+    
+    def __search_class_in_line(self, line):
         search = re.findall(r"(class[a-zA-Z0-9_-]*=|class[a-zA-Z0-9_-]* = |class[a-zA-Z0-9_-]* == )\"([\s\wa-zA-Z0-9_-]*)\"", line)  # 
         # Check for js script
         if len(search) == 0:
@@ -32,7 +40,7 @@ class HtmlFileReader():
             class_list = " ".join(sec_search).split(" ")
             return class_list
 
-    def search_id_in_line(self, line):
+    def __search_id_in_line(self, line):
         search = re.findall(r"id=\"([\s\wa-zA-Z0-9_-]*)\"", line)  # 
         id_list = " ".join(search).split(" ")
         # Check for js script
@@ -42,15 +50,3 @@ class HtmlFileReader():
 
         if len(search) >= 1:
             return id_list
-
-    def read_file_and_collect_att(self, file_path):
-        att_found_html = []
-        with open(file_path, mode='r',encoding='UTF-8') as html_file:
-            for line in html_file:
-                if "class" in line:
-                    att_found_html.append(self.search_class_in_line(line))
-                if "id=" in line:
-                    att_found_html.append(self.search_id_in_line(line))
-            print(f"HTML file ({file_path}) scanned for classes and ids.")
-        new_class_list = sum(att_found_html, [])
-        return new_class_list
