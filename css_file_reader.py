df77ba6dd489e2553e6838b954949258714918c1
RenanBa <renanbarbosadesouza@gmail.com>
2024-10-22 15:28:36 -0700

    Update css blocks into Dict. Target attr are removed from block successfully

--- css_file_reader.py
+++ css_file_reader.py
@@ -4,6 +4,7 @@ import re
 class CssFileReader():
 
     def read_file(self, target_dir, att_list):
+        print("============")
         # What does read_file do:
         # 1 - Read and save each line in a dictionary type {1: line, 2: line}
         # 2 - Check each line for regex pattern for classes and ids
@@ -20,11 +21,10 @@ class CssFileReader():
         remove_block = False
         count = 1
         with open(target_dir, mode='r',encoding='UTF-8') as target_file:
-            css_blocks = []
+            css_blocks = {"target_line": []}
             block_track = False
             for line in target_file:
                 loaded_file[count] = line
-                print("============")
                 
                 # Check for css comment blocks
                 if line[:2] == "/*":
@@ -32,17 +32,19 @@ class CssFileReader():
                 
                 if not comment_block:
                     search = re.findall(r"\.[a-zA-Z0-9_-]*,*|\#[a-zA-Z0-9_-]*,*", line)
-                    print(search)
-                    print(f"SEarch result: {search}")
+                    # print(f"SEarch result: {search}")
 
                     # Check for characters that opens a css block
-                    if "," in line or "{":
+                    if "," in line or "{" in line:
                         block_track = True
                     
                     # Collect the whole css block for later modification if needed
-                    if block_track:
-                        css_blocks.append({"line_num": count, "line_str": line, "target": False})
 
+                    if block_track:
+                        # print(f"LIne: {count} | {line}")
+                        css_blocks[count] = {"line_str": line, "att": []}
+
+                    # print(css_blocks)
                     # Check if there are matches
                     if len(search) >= 1:
                         # Go over each match found in the current line
@@ -54,17 +56,13 @@ class CssFileReader():
                             # Check if the att is in the html files attribute list
                             if att not in att_list:
                                 remove_block = True
-                                css_blocks[-1]["target"] = True
+                                css_blocks[count]["att"].append(item)
+
+                                if count not in css_blocks["target_line"]:
+                                    css_blocks["target_line"].append(count)
+
                                 print(F"Item not in att_list: {att}")
-
-                                # need to check if the previous line has a comma or check if the current line has a comma 
-                                # at the end. In this case the comma needs to be removed 
-
-                                # Also check if there is curly brace at the end. In this case the curly brace 
-                                # will need to be moved to the end of the previous line
-                            else:
-                                print(f"Do Nothing - Item is in the att list")
-                                pass
+                            # End of att no in att_list
                         # End of for each match (found in the current line)
                     # End of if search >= 1
                 # End of if not comment_block
@@ -72,11 +70,9 @@ class CssFileReader():
                 if line[-3:-1] == "*/":
                     comment_block = False
                 
-                print(f"Is block tracking: {remove_block}")
-                print(f"Is block tracking: {line[-2:-1]}")
                 if "}" == line[-2:-1]:
                     block_track = False
-                    css_blocks = []
+                    css_blocks = {"target_line": []}
 
                 if block_track and remove_block:
                     css_remove_blocks.append(css_blocks)
@@ -85,17 +81,40 @@ class CssFileReader():
                 count += 1
             # End of for each line
         # End of With open
-        print(count)
-        print(loaded_file)
-        print(css_remove_blocks)
+
         return loaded_file, css_remove_blocks
 
                     
     def read_css_file_search_attr(self, target_dir, att_list):
         print(f"Reading file: {target_dir}")
-        att_list = ["list-round", "list-arrow", "error-page", "error-body", "btn", "no-padding", "gap-60", "gap-40"]
+        att_list = ["list-check", "list-round", "btn", "no-padding", "gap-60", "gap-40", "gap-30"]
         loaded_file, css_remove_blocks = self.read_file(target_dir, att_list)
+        # print(css_remove_blocks)
+        for block in css_remove_blocks:
+            print(block["target_line"])
+            line_number_list = block["target_line"]
+            line_data = block[line_number_list[0]]
+            line = line_data["line_str"][:-1]
+            print(line_data)
+            # print()
+            list_line = line.split(" ")
+            for attr in line_data["att"]:
+                
+                print(f"attr: {attr}")
+                print(f"list_line: {list_line}")
+                print(list_line)
+                if attr in list_line:
+                    list_line.remove(attr)
+                    print(True)
+                print(f"list_line after: {list_line}")
 
+            # print(css_remove_blocks[1][line])
+
+        # need to check if the previous line has a comma or check if the current line has a comma 
+        # at the end. In this case the comma needs to be removed 
+
+        # Also check if there is curly brace at the end. In this case the curly brace 
+        # will need to be moved to the end of the previous line
 
 
 
